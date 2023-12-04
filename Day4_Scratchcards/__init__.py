dc9260ae2a8e9341037e08b379779ad0411a9a8d
LemonkaPix <maciejwrobel029@gmail.com>
2023-12-04 09:42:32 +0100

    Day 4 Part 2

--- Day4_Scratchcards/__init__.py
+++ Day4_Scratchcards/__init__.py
@@ -5,8 +5,10 @@ print("--- Day 4: Scratchcards ---")
 f = open("input.txt")
 input = f.read().split('\n')
 answer = 0
-
+cards = [1] * len(input)
+currCard = 0
 for line in input:
+    currCard += 1
     winNums = line.split(':')
     winNums = winNums[-1].split(" | ")[0].split(' ')
     while ("" in winNums):
@@ -17,12 +19,18 @@ for line in input:
     while ("" in myNums):
         myNums.remove("")
 
-    countOfNums = 0
-    for num in myNums:
-        if(winNums.__contains__(num)):
-            countOfNums += 1
+    for count in range(cards[currCard - 1]):
+        countOfNums = 0
+        for num in myNums:
+            if(winNums.__contains__(num)):
+                countOfNums += 1
 
-    print(math.floor(math.pow(2, countOfNums - 1)))
-    answer += math.floor(math.pow(2, countOfNums - 1))
+        for i in range(countOfNums):
+            cards[currCard + i] += 1
+    print(currCard)
 
+for card in cards:
+    answer += card
+
+print(cards)
 print("Answer: ", answer)
